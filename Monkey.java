2ab55a86845f036f71b9c6199002b2b10c5cce51
Marian-Lin <350862694@gapps.yrdsb.ca>
2022-06-07 09:09:25 -0400

    ninth

--- Monkey.java
+++ Monkey.java
@@ -9,24 +9,48 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Monkey extends Actor
 {
     GreenfootSound monkeySound = new GreenfootSound("monkey.wav");
-    GreenfootImage[] idle = new GreenfootImage[8];
+    GreenfootImage[] idlea = new GreenfootImage[8];
+    GreenfootImage[] idled = new GreenfootImage[8];
     
+    String facing = "a";
     /**
      * coonstructor-the cose that gets run one time whenobject is created
      */
     public Monkey()
     {
         
-        for(int i=0;i < idle.length ;i++)
+        for(int i=0;i < idlea.length ;i++)
         {
-            idle[i] = new GreenfootImage("images/monkey" + i + ".png");
-            idle[i].scale(50,50);
-            cycle++;
-            setImage(idle[cycle%(idle.length-1)]);
+            idlea[i] = new GreenfootImage("images/monkey" + i + ".png");
+            idlea[i].scale(50,50);
+            
         }
-        
+        for(int i=0;i < idled.length ;i++)
+        {
+            idled[i] = new GreenfootImage("images/monkey" + i + ".png");
+            idlea[i].mirrorHorizontally();
+            idled[i].scale(50,50);
+            
+        }
+        setImage(idled[0]);
     }
-    
+    /**
+     * animate the monkey
+     */
+    int imageIndex = 0;
+    public void animateMonkey()
+    {
+        if(facing.equals("a"))
+        {
+            setImage(idlea[imageIndex]);
+            imageIndex = (imageIndex + 1) % idlea.length;
+        }
+        else
+        {
+            setImage(idled[imageIndex]);
+            imageIndex = (imageIndex + 1) % idled.length;
+        }
+    }
     /** 
      * Act - do whatever the Monkey wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -38,12 +62,12 @@ public class Monkey extends Actor
         if(Greenfoot.isKeyDown("a"))
         {
             move(-3);
-            
+            facing = "d";
         }
         if(Greenfoot.isKeyDown("d"))
         {
             move(3);
-            
+            facing ="a";
         }
         if(Greenfoot.isKeyDown("w"))
         {
